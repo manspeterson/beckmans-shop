00deb933a3e030c8dfc81a2fd2af9f086d51105d
Måns Peterson <mp@mbp.local>
2017-10-20 11:09:33 +0200

    nothing

--- json_builder.py
+++ json_builder.py
@@ -24,21 +24,26 @@ for user in next(os.walk(base))[1]:
 				userInfo = yaml.load(f)
 				users[user] = userInfo
 				f.close();
-			productsPath = base + os.sep + user + os.sep + "products"
-			for product in next(os.walk(productsPath))[1]:
-				with open(productsPath + os.sep + product + "/info.txt", 'r') as f:
-					d = "".join(f.readlines()).split("---\n")
-					productInfo = yaml.load(d[0])
-					productInfo["caption"] = d[1]
-					productInfo["author"] = user
-					productInfo["images"] = []
-					f.close()
-				path = next(os.walk(productsPath + os.sep + product))[0].replace(os.getcwd(),'')
-				for product in next(os.walk(productsPath + os.sep + product))[2]:
-					if product.endswith(".jpg") or product.endswith('.png') or product.endswith('.gif'):
-						productInfo["images"].append(path + product)
-						# https://images.weserv.nl/?url=henriknygren.se/site/assets/files/2105/barberosgerby_bok_03.1696x0.jpg&w=800&il&q=90
-				products.append(productInfo)
+			productsPath = base + os.sep + user + os.sep + "online_products"
+			if os.path.exists(productsPath):
+				for product in next(os.walk(productsPath))[1]:
+					with open(productsPath + os.sep + product + "/info.txt", 'r') as f:
+						d = "".join(f.readlines()).split("---\n")
+						productInfo = yaml.load(d[0])
+						productInfo["caption"] = d[1]
+						productInfo["author"] = user
+						productInfo["images"] = []
+						f.close()
+					path = next(os.walk(productsPath + os.sep + product))[0].replace(os.getcwd(),'')
+					for product in next(os.walk(productsPath + os.sep + product))[2]:
+						if product.endswith(".jpg") or product.endswith('.png') or product.endswith('.gif'):
+
+							
+							product = urllib.quote(product)
+							# print product
+							productInfo["images"].append(path + product)
+							# https://images.weserv.nl/?url=henriknygren.se/site/assets/files/2105/barberosgerby_bok_03.1696x0.jpg&w=800&il&q=90
+					products.append(productInfo)
 data =  json.dumps({"users" : users, "products" : products}, ensure_ascii=False).encode('utf8')
 
 with open('data.json', 'w') as f:
