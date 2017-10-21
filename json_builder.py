5876dcc07d045e94a53bec022fea1b2f860e082c
Måns Peterson <MP@server.manspeterson.se>
2017-10-21 17:13:56 +0200

    added new images, + design

--- json_builder.py
+++ json_builder.py
@@ -31,9 +31,11 @@ for user in next(os.walk(base))[1]:
 						with open(productsPath + os.sep + product, 'r') as f:
 							d = "".join(f.readlines()).split("---\n")
 							productInfo = yaml.load(d[0])
-							productInfo["caption"] = d[1]
-							productInfo["author"] = user
-							productInfo["images"] = []
+							productInfo["beskrivning"] = d[1]
+							productInfo["designer"] = userInfo["namn"]
+							productInfo["designer-slug"] = user
+							productInfo["titel-slug"] = product.replace('.txt', '')
+							productInfo["bilder"] = []
 							f.close()
 						path = next(os.walk(productsPath + os.sep + product.replace('.txt', '')))[0].replace(os.getcwd(),'')
 						for productImage in next(os.walk(productsPath + os.sep + product.replace('.txt', '')))[2]:
@@ -41,11 +43,16 @@ for user in next(os.walk(base))[1]:
 
 								productImageName = urllib.quote(productImage)
 								# print product
-								productInfo["images"].append(path[1:] + os.sep + productImageName)
+								productInfo["bilder"].append(path[1:] + os.sep + productImageName)
 								# https://images.weserv.nl/?url=henriknygren.se/site/assets/files/2105/barberosgerby_bok_03.1696x0.jpg&w=800&il&q=90
-						if len(productInfo["images"]) != 0:
+						if len(productInfo["bilder"]) != 0:
 							products.append(productInfo)
-data =  json.dumps({"users" : users, "products" : products}, ensure_ascii=False).encode('utf8')
+tags = set();
+for p in products:
+	for t in p["taggar"].split(', '):
+		tags.add(t.lower())
+
+data =  json.dumps({"users" : users, "products" : products, "taggar" : list(tags)},encoding='utf8', ensure_ascii=False).encode('utf8')
 
 with open('js/data.js', 'w') as f:
 	f.write("var data = " + data + ";")
